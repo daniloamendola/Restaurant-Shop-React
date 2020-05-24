cf721a2cc8fdf3b02231e2048650a1bfa8a5a061
Danilo Amendola <daniloamendola@gmail.com>
2020-05-24 15:24:30 +0200

    added image

---
+++ json-server/public/precache-manifest.834c00bacbf49da363c95440f822d26a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022f71c53ef7eda2c1b2139ed99ddbeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd548b671c6c2476d56",
+    "url": "/static/css/2.9a81eafb.chunk.css"
+  },
+  {
+    "revision": "f467f064b75af28b8d94",
+    "url": "/static/css/main.b1c5511e.chunk.css"
+  },
+  {
+    "revision": "0bd548b671c6c2476d56",
+    "url": "/static/js/2.393c9f25.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.393c9f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f467f064b75af28b8d94",
+    "url": "/static/js/main.fce8de7d.chunk.js"
+  },
+  {
+    "revision": "d5b11a12513e36db8a95",
+    "url": "/static/js/runtime-main.97c4d7e5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
